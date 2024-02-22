ec4564c91ac8a3bb0ffbc59117ba019db79d8bd0
ALOMGIRHOSSAIN2024 <160077582+ALOMGIRHOSSAIN2024@users.noreply.github.com>
2024-02-22 18:12:18 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+# Run `python main.py` in the terminal
+
+#print("Acc 341 class on Feb 21")
+
+#first_name = input('What is your name:')
+
+#print('ok, your first name is',first_name)
+
+#age = ''
+
+#try:
+  #age = float(input('How old are you: '))
+
+#except:
+  #print('Error Occurred')
+#print('You are', age, 'years old.')
+#print('In five years you will be', age + 5 'years old.')
+
+#num1 = float(input('Type a number: '))
+
+#print(num1/0)
+
+#prin('seven')
+
+
+#try:
+  #age = float (input('How old are you? '))
+  #print('in two years, you will be', age + 3)
+#except:
+  #print('error')  
+
+my_list = ["apple", "banana", "cherry"]
+print(type(my_list))
+
+john_doe_grades = [85, 60, 90, 75]
+print(john_doe_grades)
+
+#john_doe_quiz1 = 85
+#john_doe_quiz2 = 60.0
+#john_doe_quiz3 = "90"
+#john_doe_quiz4 = 75
+
+#print(type(john_doe_quiz1))
+#print(type(john_doe_quiz2))
+#print(type(john_doe_quiz3))
+
+
+john_doe_grades = [85, 60, 90, 75]
+john_doe_grades.append(95) = 95
+john_doe_grades.pop(0)
+print(john_doe_grades)
+
